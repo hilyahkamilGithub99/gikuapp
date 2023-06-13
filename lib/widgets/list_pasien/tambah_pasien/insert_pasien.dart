1ccd0f33d008ceb9e19c0c80b695b3d7f4134e8e
hilyahkamil <hilyahkamilah16@gmail.com>
2023-06-13 20:04:58 +0700

    pembuatan data table

--- lib/widgets/list_pasien/tambah_pasien/insert_pasien.dart
+++ lib/widgets/list_pasien/tambah_pasien/insert_pasien.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class DataPasien {
   final String patientName;
@@ -45,13 +46,30 @@ class _ViewJsonState extends State<ViewJson> {
   }
 
   Future<void> getDataPasien() async {
-    // Implement your logic to fetch data from the server here
-    // For example:
-    // var response = await http.get("your_api_endpoint");
-    // Parse the response and update the listDataPasien accordingly
-    // setState(() {
-    //   listDataPasien = ...;
-    // });
+    var response = await http.get(Uri.parse("http://10.0.134.127:8080/gikuapp/list_pasien.php"));
+    if (response.statusCode == 200) {
+      var jsonData = jsonDecode(response.body);
+      print(jsonData); // Print the response for debugging purposes
+      if (jsonData is List) {
+        setState(() {
+          listDataPasien = jsonData
+              .map((data) => DataPasien(
+                    patientName: data["patient_name"],
+                    patientAge: data["patient_age"],
+                    patientAddress: data["patient_address"],
+                    treatmentType: data["treatment_type"],
+                    timeSchedule: data["time_schedule"],
+                    description: data["description"],
+                    doctorId: data["doctor_id"],
+                  ))
+              .toList();
+        });
+      } else {
+        print("Data is not in the expected format");
+      }
+    } else {
+      print("Failed to load data");
+    }
   }
 
   Future<void> insertDataPasien() async {
@@ -92,206 +110,196 @@ class _ViewJsonState extends State<ViewJson> {
       body: Column(
         children: [
           Expanded(
-            child: ListView(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              shrinkWrap: true,
-              physics: ClampingScrollPhysics(),
-              children: [
-                Text("Form Daftar"),
-                Divider(
-                  thickness: 1,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.4,
-                        ),
-                        child: TextField(
-                          controller: _controllerPatientName,
-                          decoration: InputDecoration(hintText: "Nama Lengkap"),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.4,
-                        ),
-                        child: TextField(
-                          controller: _controllerPatientAddress,
-                          decoration: InputDecoration(hintText: "Alamat"),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.4,
-                        ),
-                        child: TextField(
-                          controller: _controllerPatientAge,
-                          decoration: InputDecoration(hintText: "Umur"),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.4,
-                        ),
-                        child: TextField(
-                          controller: _controllerTreatmentType,
-                          decoration: InputDecoration(hintText: "Jenis Penanganan"),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.4,
-                        ),
-                        child: DropdownButtonFormField<String>(
-                          value: _selectedTime,
-                          onChanged: (value) {
-                            setState(() {
-                              _selectedTime = value;
-                              _controllerTimeSchedule.text = value ?? '';
-                            });
-                          },
-                          items: [
-                            DropdownMenuItem(
-                              value: "Pukul 16.00 - 16.30",
-                              child: Text("Pukul 16.00 - 16.30"),
-                            ),
-                            DropdownMenuItem(
-                              value: "Pukul 16.30 - 17.00",
-                              child: Text("Pukul 16.30 - 17.00"),
-                            ),
-                            DropdownMenuItem(
-                              value: "Pukul 17.00 - 17.30",
-                              child: Text("Pukul 17.00 - 17.30"),
-                            ), 
-                            DropdownMenuItem(
-                              value: "Pukul 17.30 - 18.00",
-                              child: Text("Pukul 17.30 - 18.00"),
-                            ),
-                            DropdownMenuItem(
-                              value: "Pukul 18.00 - 18.30",
-                              child: Text("Pukul 18.00 - 18.30"),
-                            ),
-                            DropdownMenuItem(
-                              value: "Pukul 18.30 - 19.00",
-                              child: Text("Pukul 18.30 - 19.00"),
-                            ),
-                            DropdownMenuItem(
-                              value: "Pukul 19.00 - 19.30",
-                              child: Text("Pukul 19.00 - 19.30"),
-                            ),
-                            DropdownMenuItem(
-                              value: "Pukul 19.30 - 20.00",
-                              child: Text("Pukul 19.30 - 20.00"),
-                            ),
-                            DropdownMenuItem(
-                              value: "Pukul 20.00 - 20.30",
-                              child: Text("Pukul 20.00 - 20.30"),
-                            ),
-                            // Add more DropdownMenuItem if needed
-                          ],
-                          decoration: InputDecoration(
-                            hintText: "Pilih Jam",
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                children: [
+                  Text("Form Daftar"),
+                  Divider(
+                    thickness: 1,
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * 0.4,
+                          ),
+                          child: TextField(
+                            controller: _controllerPatientName,
+                            decoration: InputDecoration(hintText: "Nama Lengkap"),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.4,
-                        ),
-                        child: TextField(
-                          controller: _controllerDescription,
-                          decoration: InputDecoration(hintText: "Deskripsi"),
+                      SizedBox(width: 10),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * 0.4,
+                          ),
+                          child: TextField(
+                            controller: _controllerPatientAddress,
+                            decoration: InputDecoration(hintText: "Alamat"),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.4,
-                        ),
-                        child: TextField(
-                          controller: _controllerDoctorId,
-                          decoration: InputDecoration(hintText: "ID Dokter"),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print(_controllerPatientAge.text);
-                    insertDataPasien();
-                  },
-                  child: Text(
-                    "Daftar",
-                    style: TextStyle(fontSize: 18),
+                    ],
                   ),
-                ),
-              ],
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * 0.4,
+                          ),
+                          child: TextField(
+                            controller: _controllerPatientAge,
+                            decoration: InputDecoration(hintText: "Umur"),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * 0.4,
+                          ),
+                          child: TextField(
+                            controller: _controllerTreatmentType,
+                            decoration: InputDecoration(hintText: "Jenis Penanganan"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * 0.4,
+                          ),
+                          child: DropdownButtonFormField<String>(
+                            value: _selectedTime,
+                            onChanged: (value) {
+                              setState(() {
+                                _selectedTime = value;
+                                _controllerTimeSchedule.text = value ?? '';
+                              });
+                            },
+                            items: [
+                              DropdownMenuItem(
+                                value: "Pukul 16.00 - 16.30",
+                                child: Text("Pukul 16.00 - 16.30"),
+                              ),
+                              DropdownMenuItem(
+                                value: "Pukul 16.30 - 17.00",
+                                child: Text("Pukul 16.30 - 17.00"),
+                              ),
+                              // Add more DropdownMenuItem if needed
+                            ],
+                            decoration: InputDecoration(
+                              hintText: "Pilih Jam",
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * 0.4,
+                          ),
+                          child: TextField(
+                            controller: _controllerDescription,
+                            decoration: InputDecoration(hintText: "Deskripsi"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * 0.4,
+                          ),
+                          child: TextField(
+                            controller: _controllerDoctorId,
+                            decoration: InputDecoration(hintText: "ID Dokter"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: insertDataPasien,
+                    child: Text("Daftar"),
+                  ),
+                  SizedBox(height: 20),
+                  Text("Daftar Antrian"),
+                  Divider(
+                    thickness: 1,
+                  ),
+                  SizedBox(height: 10),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    itemCount: listDataPasien.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(listDataPasien[index].patientName),
+                        subtitle: Text(listDataPasien[index].timeSchedule),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            deleteDataPasien(listDataPasien[index].patientName);
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  Future<void> deleteDataPasien(String patientName) async {
+    var response = await http.post(
+      Uri.parse("http://10.0.134.127:8080/gikuapp/delete.php"),
+      body: {
+        "patient_name": patientName,
+      },
+    );
+    print(response.body);
+    if (response.statusCode == 200) {
+      getDataPasien();
+    }
   }
 }
